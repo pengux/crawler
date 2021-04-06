4a5750174d6ff7df8096de68c963501915f9d0e5
Peter Nguyen <peter@mictis.com>
2021-04-06 19:02:52 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module crawler
+
+go 1.15
+
+require (
+	github.com/andybalholm/cascadia v1.2.0
+	github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/net v0.0.0-20201216054612-986b41b23924
+)
---
+++ main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/andybalholm/cascadia"
+	sitemap "github.com/oxffaa/gopher-parse-sitemap"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/net/html"
+)
+
+var (
+	menuLinkSelector = cascadia.MustCompile("#defaultmenu a")
+	client           = http.Client{}
+)
+
+func main() {
+	app := &cli.App{
+		Name:   "crawler",
+		Usage:  "Crawl URLs so it can be pre-cached",
+		Action: run,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "mode",
+				Aliases:     []string{"m"},
+				Usage:       "The mode for crawling, either 'sitemap' or 'links', defaults to 'sitemap'",
+				DefaultText: "sitemap",
+			},
+			&cli.StringSliceFlag{
+				Name:    "site",
+				Aliases: []string{"s"},
+				Usage:   "The site to crawl",
+			},
+			&cli.IntFlag{
+				Name:    "concurrency",
+				Aliases: []string{"n"},
+				Usage:   "Number of concurrent crawling processes",
+				Value:   10,
+			},
+			&cli.IntFlag{
+				Name:    "timeout",
+				Aliases: []string{"t"},
+				Usage:   "Request timeout",
+				Value:   10,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	sites := c.StringSlice("site")
+	client = http.Client{
+		Timeout: time.Duration(c.Int("timeout")) * time.Second,
+	}
+	concurrency := c.Int("concurrency")
+
+	var err error
+	for _, site := range sites {
+		if c.String("mode") == "links" {
+			err = crawlLinks(site, concurrency)
+		} else {
+			err = crawlSitemap(site+"/sitemap.xml", concurrency)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func crawlSitemap(url string, concurrency int) error {
+	log.Printf("crawling sitemap index '%s'", url)
+	var sem = make(chan int, concurrency)
+
+	return sitemap.ParseFromSite(url, func(e sitemap.Entry) error {
+		sem <- 1
+		go func(url string) {
+			defer func() {
+				<-sem
+			}()
+
+			err := do(url)
+			if err != nil {
+				log.Printf("[ERROR] could not crawl URL '%s': %v", url, err)
+			}
+		}(e.GetLocation())
+		return nil
+	})
+}
+
+func crawlLinks(startURL string, concurrency int) error {
+	req, err := http.NewRequest(http.MethodGet, startURL, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	// Get the links in main menu
+	htmlDoc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	menuLinks := menuLinkSelector.MatchAll(htmlDoc)
+	var sem = make(chan int, concurrency)
+
+	for _, link := range menuLinks {
+		for _, attr := range link.Attr {
+			if attr.Key != "href" {
+				continue
+			}
+
+			sem <- 1
+			go func(url string) {
+				defer func() {
+					<-sem
+				}()
+
+				if !strings.HasPrefix(url, "http") {
+					if !strings.HasPrefix(url, "/") {
+						url = "/" + url
+					}
+					url = startURL + url
+				}
+				if !strings.HasPrefix(url, startURL) {
+					return
+				}
+
+				err := do(url)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(attr.Val)
+		}
+	}
+
+	return nil
+}
+
+func do(url string) error {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	start := time.Now()
+	_, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	log.Printf("response time: %d ms for requesting %s\n", time.Since(start).Milliseconds(), url)
+
+	return nil
+}
